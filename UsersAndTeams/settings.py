732abb4f122cc0c66bd49bd26f7abc7799c1121b
Grade <aborecevskij94@gmail.com>
2024-06-25 14:42:06 +0300

    upd: database credentials

--- UsersAndTeams/settings.py
+++ UsersAndTeams/settings.py
@@ -11,6 +11,18 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s environment variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +88,12 @@ WSGI_APPLICATION = 'UsersAndTeams.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_variable("POSTGRES_DB"),
+        'USER': get_env_variable("POSTGRES_USER"),
+        'PASSWORD': get_env_variable("POSTGRES_PASSWORD"),
+        'HOST': get_env_variable("POSTGRES_HOST"),
+        'PORT': get_env_variable("POSTGRES_PORT")
     }
 }
 
