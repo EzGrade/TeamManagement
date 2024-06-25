0e3f325ebee35a611d469a5d5439036b5dacb1df
Grade <aborecevskij94@gmail.com>
2024-06-25 13:29:27 +0300

    add: post method in User view to create user

--- team_management/views.py
+++ team_management/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from team_management.serializers import UserSerializer
+
+
+class UserView(APIView):
+
+    @swagger_auto_schema(request_body=UserSerializer)
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
