3414e1e23e1cd139d492adb942b2a97397e3416b
Grade <aborecevskij94@gmail.com>
2024-06-25 13:28:50 +0300

    upd: registered UserProfile model in django admin

--- team_management/admin.py
+++ team_management/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from team_management.models import UserProfile
+
+
 # Register your models here.
+@admin.register(UserProfile)
+class UserProfile(admin.ModelAdmin):
+    pass
