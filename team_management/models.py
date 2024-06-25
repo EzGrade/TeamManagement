378be7f68b4d67865eb14bedd5b3421e2e238804
Grade <aborecevskij94@gmail.com>
2024-06-25 13:04:48 +0300

    add: user and team models

--- team_management/models.py
+++ team_management/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+class UserProfile(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    team = models.OneToOneField('Team', on_delete=models.SET_NULL, null=True)
+
+    @property
+    def full_name(self):
+        return self.first_name + ' ' + self.last_name
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    users = models.ManyToManyField(UserProfile)
+
+    def __str__(self):
+        return self.name
