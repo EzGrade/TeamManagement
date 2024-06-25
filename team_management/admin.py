f416e07ca8c05273da225c132669e06b7e7c1428
Grade <aborecevskij94@gmail.com>
2024-06-25 13:54:10 +0300

    upd: registered Team model

--- team_management/admin.py
+++ team_management/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from team_management.models import UserProfile
+from team_management.models import UserProfile, Team
 
 
 # Register your models here.
 @admin.register(UserProfile)
 class UserProfile(admin.ModelAdmin):
-    pass
+    list_display = ['pk', 'full_name', 'email', 'team']
+
+
+@admin.register(Team)
+class Team(admin.ModelAdmin):
+    list_display = ['pk', 'name']
+    filter_horizontal = ['users']
