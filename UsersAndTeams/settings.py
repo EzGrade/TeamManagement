ac2ce6f83e1e117a5967ca7297efb197512bf01c
Grade <aborecevskij94@gmail.com>
2024-06-25 17:00:20 +0300

    upd: removed dotenv

--- UsersAndTeams/settings.py
+++ UsersAndTeams/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-import dotenv
-
-dotenv.load_dotenv(dotenv.find_dotenv())
 
 from django.core.exceptions import ImproperlyConfigured
 
