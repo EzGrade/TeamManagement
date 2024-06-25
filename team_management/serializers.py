2b34a1f35755eddc2147a259a293fe7f619d9696
Grade <aborecevskij94@gmail.com>
2024-06-25 13:54:41 +0300

    add: TeamSerializer

--- team_management/serializers.py
+++ team_management/serializers.py
@@ -6,6 +6,7 @@ class UserSerializer(serializers.Serializer):
     first_name = serializers.CharField()
     last_name = serializers.CharField()
     email = serializers.EmailField()
+    team = serializers.PrimaryKeyRelatedField(queryset=Team.objects.all(), required=False)
 
     class Meta:
         model = UserProfile
@@ -23,4 +24,47 @@ class UserSerializer(serializers.Serializer):
             "first_name": instance.first_name,
             "last_name": instance.last_name,
             "email": instance.email
+        }
+
+
+class TeamSerializer(serializers.Serializer):
+    pk = serializers.IntegerField(required=False)
+    name = serializers.CharField()
+    users = serializers.ListField(child=serializers.PrimaryKeyRelatedField(queryset=UserProfile.objects.all()),
+                                  required=False)
+
+    class Meta:
+        model = Team
+        fields = ['pk', 'name', 'users']
+
+    def create(self, validated_data):
+        if Team.objects.filter(name=validated_data['name']).exists():
+            raise serializers.ValidationError('Team with this name already exists')
+
+        users = validated_data.pop('users', [])
+        team = Team.objects.create(**validated_data)
+        for user in users:
+            team.users.add(user)
+        return team
+
+    def update(self, instance, validated_data):
+        instance_name = validated_data.get('name', None)
+        if instance_name and Team.objects.filter(name=instance_name).exclude(pk=instance.pk).exists():
+            raise serializers.ValidationError('Team with this name already exists')
+
+        instance.name = validated_data.get('name', instance.name)
+
+        users = validated_data.get('users', [])
+        if isinstance(users, list):
+            instance.users.clear()
+        for user in users:
+            instance.users.add(user)
+        instance.save()
+        return instance
+
+    def to_representation(self, instance: Team):
+        return {
+            "pk": instance.pk,
+            "name": instance.name,
+            "users": UserSerializer(instance.users.all(), many=True).data
         }
