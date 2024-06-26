e9710868ad47cc8561b67772e189044606d2dc83
Grade <aborecevskij94@gmail.com>
2024-06-26 18:04:08 +0300

    fix: old tests

--- team_management/tests.py
+++ team_management/tests.py
@@ -20,7 +20,7 @@ class TestUserProfile(TestCase):
 
     def test_get_user(self):
         response = self.client.post('/api/user/', self.user_data)
-        user_id = response.data['pk']
+        user_id = response.data['id']
         response = self.client.get(f'/api/user/{user_id}/')
         self.assertEqual(response.status_code, 200)
 
@@ -43,49 +43,46 @@ class TestTeam(TestCase):
         self.assertEqual(response.status_code, 201)
 
     def test_create_team_with_users(self):
-        self.team_data['users'] = [self.user['pk']]
+        self.team_data['users'] = [self.user['id']]
         response = self.client.post('/api/team/', self.team_data, content_type='application/json')
         self.assertEqual(response.status_code, 201)
-        self.assertIn(self.user, response.data['users'])
+        self.assertIn(self.user['id'], response.data['users'])
 
     def test_create_team_with_existing_name(self):
-        response_1 = self.client.post('/api/team/', self.team_data)
+        self.client.post('/api/team/', self.team_data)
         response = self.client.post('/api/team/', self.team_data)
         self.assertEqual(response.status_code, 400)
 
     def test_get_team(self):
         response = self.client.post('/api/team/', self.team_data)
-        team_id = response.data['pk']
+        team_id = response.data['id']
         response = self.client.get(f'/api/team/{team_id}/')
         self.assertEqual(response.status_code, 200)
 
     def test_update_team(self):
         response = self.client.post('/api/team/', self.team_data, content_type='application/json')
-        team_id = response.data['pk']
-        self.team_data['pk'] = team_id
+        team_id = response.data['id']
         self.team_data['name'] = 'Team 2'
-        response = self.client.put('/api/team/', self.team_data, content_type='application/json')
-        self.assertEqual(response.status_code, 201)
+        response = self.client.put(f'/api/team/{team_id}/', self.team_data, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['name'], 'Team 2')
 
     def test_remove_users_from_team(self):
-        self.team_data['users'] = [self.user['pk']]
+        self.team_data['users'] = [self.user['id']]
         response = self.client.post('/api/team/', self.team_data)
-        team_id = response.data['pk']
-        self.team_data['pk'] = team_id
+        team_id = response.data['id']
         self.team_data['users'] = []
-        response = self.client.put('/api/team/', self.team_data, content_type='application/json')
-        self.assertEqual(response.status_code, 201)
+        response = self.client.put(f'/api/team/{team_id}/', self.team_data, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['users'], [])
 
     def test_add_user_to_team(self):
         response = self.client.post('/api/team/', self.team_data)
-        team_id = response.data['pk']
-        self.team_data['pk'] = team_id
-        self.team_data['users'] = [self.user['pk']]
-        response = self.client.put('/api/team/', self.team_data, content_type='application/json')
-        self.assertEqual(response.status_code, 201)
-        self.assertIn(self.user, response.data['users'])
+        team_id = response.data['id']
+        self.team_data['users'] = [self.user['id']]
+        response = self.client.put(f'/api/team/{team_id}/', self.team_data, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(self.user['id'], response.data['users'])
 
 
 if __name__ == "__main__":
