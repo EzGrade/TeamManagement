6ff163846829cb20bf06805f9012cd3de9a4d1e5
Grade <aborecevskij94@gmail.com>
2024-06-26 18:04:39 +0300

    add: put method to team endpoint

--- team_management/urls.py
+++ team_management/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from .views import UserView, TeamView
 
 urlpatterns = [
-    path('user/<int:pk>/', UserView.as_view({'get': 'get'})),
+    path('user/<int:pk>/', UserView.as_view({'get': 'retrieve'})),
     path('user/', UserView.as_view({'post': 'create'})),
-    path('team/<int:pk>/', TeamView.as_view({'get': 'get'})),
+    path('team/<int:pk>/', TeamView.as_view({'get': 'retrieve', 'put': 'update'})),
     path('team/', TeamView.as_view({'post': 'create'})),
-    path('team/', TeamView.as_view({'put': 'put'})),
 ]
