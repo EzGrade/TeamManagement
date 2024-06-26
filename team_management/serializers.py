caa1128d81769e314a4659c9a008b2346404a401
Grade <aborecevskij94@gmail.com>
2024-06-26 13:01:44 +0300

    upd: dont create team if this name exists

--- team_management/serializers.py
+++ team_management/serializers.py
@@ -20,44 +20,31 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class TeamSerializer(serializers.ModelSerializer):
-    users = serializers.ListField(child=serializers.IntegerField(), required=False)
-
     class Meta:
         model = Team
         fields = ['pk', 'name', 'users']
 
     def create(self, validated_data):
-        if Team.objects.filter(name=validated_data['name']).exists():
-            raise serializers.ValidationError('Team with this name already exists')
+        if Team.objects.filter(name=validated_data.get('name')).exists():
+            raise serializers.ValidationError("Team with this name already exists")
         users = validated_data.pop('users', [])
         team = Team.objects.create(**validated_data)
-        for user in users:
-            user_obj = UserProfile.objects.get(pk=int(user))
-            user_obj.team = team
-            user_obj.save()
+        team.users.set(users)
         return team
 
-    def update(self, instance, validated_data):
-        instance_name = validated_data.get('name', None)
-        if instance_name and Team.objects.filter(name=instance_name).exclude(pk=instance.pk).exists():
-            raise serializers.ValidationError('Team with this name already exists')
-
+    def update(self, instance: Team, validated_data):
+        if Team.objects.filter(name=validated_data.get('name')).exists() and instance.name != validated_data.get(
+                'name'):
+            raise serializers.ValidationError("Team with this name already exists")
         instance.name = validated_data.get('name', instance.name)
-
-        users = validated_data.get('users', [])
-        if isinstance(users, list):
-            users_list = UserProfile.objects.filter(team=instance)
-            for user in users_list:
-                user.team = None
-                user.save()
-        else:
-            for user in users:
-                instance.users.add(user)
-        instance.save()
+        users = validated_data.pop('users', None)
+        instance = super().update(instance, validated_data)
+        if users is not None:
+            instance.users.set(users)
         return instance
 
     def to_representation(self, instance: Team):
-        users = UserProfile.objects.filter(team=instance)
+        users = instance.users
         users = UserSerializer(users, many=True).data
         return {
             "pk": instance.pk,
