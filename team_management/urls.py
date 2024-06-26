26c8cdc2710d54b9e49a1e48fb1ef10b1ae5a1eb
Grade <aborecevskij94@gmail.com>
2024-06-26 18:09:27 +0300

    add: endpoint to get list of all users in team

--- team_management/urls.py
+++ team_management/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('user/<int:pk>/', UserView.as_view({'get': 'retrieve'})),
     path('user/', UserView.as_view({'post': 'create'})),
     path('team/<int:pk>/', TeamView.as_view({'get': 'retrieve', 'put': 'update'})),
+    path('team/<int:pk>/users/', TeamView.as_view({'get': 'get_users'})),
     path('team/', TeamView.as_view({'post': 'create'})),
 ]
